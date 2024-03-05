96fc31b21d8fc1a08c38372fd9d34f1ec16a36bf
Maksym Protsenko <maksim20032@hotmail.com>
2024-03-04 22:39:16 -0300

    Update CitiesData.cs

--- WeatherDataMongo.Data/CitiesData.cs
+++ WeatherDataMongo.Data/CitiesData.cs
@@ -16,7 +16,7 @@ namespace WeatherDataMongo.Data
         /*
          TODO: 
         Move connection to settings
-        Optimize the codekhgkh tests 2
+        Optimize the codekhgkh tests 23
          */
 
         private readonly string _rootPath;
